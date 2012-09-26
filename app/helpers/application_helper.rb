238587436e6ddc77a887b2cbba725befa9f3ae1d
Grant Speelman <grant.speelman@unboxedconsulting.com>
2012-09-26 15:40:46 +0200

    update shinka ads

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
   def shinka_ad
     result = ""
     begin
-    ads = ActiveSupport::JSON.decode(open("http://ox-d.shinka.sh/ma/1.0/arj?auid=290386c.age=#{@mxit_profile.age}&c.gender=#{@mxit_profile.gender}&c.country=#{@mxit_profile.country}").read)
+    ads = ActiveSupport::JSON.decode(open("http://ox-d.shinka.sh/ma/1.0/arj?auid=290386").read)
     ad = ads['ads']["ad"].sample
     result = ad["html"].html_safe
     impression = ad["creative"].first["tracking"]["impression"]
@@ -16,7 +16,7 @@ module ApplicationHelper
     end
     rescue Exception => e
       Rails.logger.error e.message
-       return result
+      return result
     end
   end
 
