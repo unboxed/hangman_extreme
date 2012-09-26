eb667caacb2799fe122e78b6698d2c1f8121d5dd
Grant Speelman <grant.speelman@unboxedconsulting.com>
2012-09-26 14:09:22 +0200

    pull shinka ads from api

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def shinka_ad
+    begin
+    ads = ActiveSupport::JSON.decode(open("http://ox-d.shinka.sh/ma/1.0/arj?auid=290386").read)
+    ad = ads['ads']["ad"].sample
+    ad["html"].html_safe
+    rescue
+       return ""
+    end
+  end
+
 end
