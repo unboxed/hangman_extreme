206fb998612b2c8452f8e0a1697668c52313a657
Grant Speelman <grant.speelman@unboxedconsulting.com>
2012-09-26 14:46:30 +0200

    update shinka ads

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,18 @@
 module ApplicationHelper
 
   def shinka_ad
+    result = ""
     begin
     ads = ActiveSupport::JSON.decode(open("http://ox-d.shinka.sh/ma/1.0/arj?auid=290386").read)
     ad = ads['ads']["ad"].sample
-    ad["html"].html_safe
-    rescue
-       return ""
+    result = ad["html"].html_safe
+    impression = ad["creative"].first["tracking"]["impression"]
+    alt = ad["creative"].first["alt"]
+    click = ad["creative"].first["tracking"]["click"]
+    "<img href=#{impression} /><a href=#{click} onclick='window.open(this.href); return false;' >#{alt}</a>".html_safe
+    rescue Exception => e
+      Rails.logger.error e.message
+       return result
     end
   end
 
