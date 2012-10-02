2d9f5f59c3f09eeef65522e76727c5ca73401499
Grant Speelman <grant.speelman@unboxedconsulting.com>
2012-10-02 15:19:32 +0200

    jenkins deploy to staging

---
+++ script/jenkins_deploy_staging.sh
@@ -0,0 +1,8 @@
+eval `ssh-agent -s`
+ssh-add
+rvm use 1.9.3@hangman_extreme
+git remote add staging git@heroku.com:tryhangmanleague.git; true
+git push staging master
+heroku run rake db:migrate --app tryhangmanleague
+heroku run rake airbrake:deploy TO=staging --app tryhangmanleague
+
