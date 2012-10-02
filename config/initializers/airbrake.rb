e6e6ca23e68c63ac4546f506999a8e43bcd34abf
Grant Speelman <grant.speelman@unboxedconsulting.com>
2012-10-02 13:51:57 +0200

    can set environment name in airbrake

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,5 +1,6 @@
 Airbrake.configure do |config|
   config.api_key = ENV['AIRBRAKE_API_KEY']
+  config.environment_name = ENV['AIRBRAKE_ENV_NAME'] if ENV['AIRBRAKE_ENV_NAME'].present?
 end if ENV['AIRBRAKE_API_KEY'].present?
 
 
