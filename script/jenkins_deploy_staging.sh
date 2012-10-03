1fce83488915d50a96619d03214948b7ef193ab6
Grant Speelman <grant.speelman@unboxedconsulting.com>
2012-10-03 11:10:57 +0200

    list keys

--- script/jenkins_deploy_staging.sh
+++ script/jenkins_deploy_staging.sh
@@ -1,7 +1,8 @@
 eval `ssh-agent -s`
 ssh-add
 rvm use 1.9.3@hangman_extreme
-git push staging master --verbose
+heroku keys
+git push staging master
 heroku run rake db:migrate --app tryhangmanleague
 heroku run rake airbrake:deploy TO=staging --app tryhangmanleague
 
