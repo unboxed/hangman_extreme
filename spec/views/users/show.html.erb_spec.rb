bbd5ffa89c056d6c4acfa3226a22f3c28d7fdbdf
Grant Speelman <grant.speelman@unboxedconsulting.com>
2012-10-03 10:05:52 +0200

    fixed todays score on user rank page

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -1,14 +1,10 @@
-
 require 'spec_helper'
 
 describe "users/show.html.erb" do
   include ViewCapybaraRendered
 
   before(:each) do
-    @user = assign(:user, stub_model(User, rank: 1,
-                                           games_won_this_week: 1,
-                                           weekly_rating: 20,
-                                           weekly_precision: 20))
+    @user = assign(:user, stub_model(User, rank: 99))
   end
 
   it "renders a list of games" do
@@ -16,6 +12,90 @@ describe "users/show.html.erb" do
     rendered.should have_content("Games Won")
     rendered.should have_content("Rating")
     rendered.should have_content("Precision")
+  end
+
+  context "Today scores" do
+
+    it "must games won" do
+      @user.should_receive(:games_won_today).and_return("111")
+      @user.should_receive(:rank).with(:games_won_today).and_return(1)
+      render
+      rendered.should have_content("111")
+      rendered.should have_content("1st")
+    end
+
+    it "must show rating" do
+      @user.should_receive(:daily_rating).and_return("112")
+      @user.should_receive(:rank).with(:daily_rating).and_return(2)
+      render
+      rendered.should have_content("112")
+      rendered.should have_content("2nd")
+    end
+
+    it "must show precision" do
+      @user.should_receive(:daily_precision).and_return("113")
+      @user.should_receive(:rank).with(:daily_precision).and_return(3)
+      render
+      rendered.should have_content("113")
+      rendered.should have_content("3rd")
+    end
+
+  end
+
+  context "Weeks scores" do
+
+    it "must games won" do
+      @user.should_receive(:games_won_this_week).and_return("1111")
+      @user.should_receive(:rank).with(:games_won_this_week).and_return(1)
+      render
+      rendered.should have_content("1111")
+      rendered.should have_content("1st")
+    end
+
+    it "must show rating" do
+      @user.should_receive(:weekly_rating).and_return("1121")
+      @user.should_receive(:rank).with(:weekly_rating).and_return(2)
+      render
+      rendered.should have_content("1121")
+      rendered.should have_content("2nd")
+    end
+
+    it "must show precision" do
+      @user.should_receive(:weekly_precision).and_return("1131")
+      @user.should_receive(:rank).with(:weekly_precision).and_return(3)
+      render
+      rendered.should have_content("1131")
+      rendered.should have_content("3rd")
+    end
+
+  end
+
+  context "Month scores" do
+
+    it "must games won" do
+      @user.should_receive(:games_won_this_month).and_return("11111")
+      @user.should_receive(:rank).with(:games_won_this_month).and_return(1)
+      render
+      rendered.should have_content("11111")
+      rendered.should have_content("1st")
+    end
+
+    it "must show rating" do
+      @user.should_receive(:monthly_rating).and_return("11211")
+      @user.should_receive(:rank).with(:monthly_rating).and_return(2)
+      render
+      rendered.should have_content("11211")
+      rendered.should have_content("2nd")
+    end
+
+    it "must show precision" do
+      @user.should_receive(:monthly_precision).and_return("11311")
+      @user.should_receive(:rank).with(:monthly_precision).and_return(3)
+      render
+      rendered.should have_content("11311")
+      rendered.should have_content("3rd")
+    end
+
   end
 
   it "should have a home page link" do
