afbedb040f12bbc3d3b582afbf0933c6689b03c5
Grant Speelman <grant.speelman@unboxedconsulting.com>
2012-10-09 09:44:59 +0200

    fixed listing users scores

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -6,11 +6,12 @@ describe "users/index" do
   before(:each) do
     @users =
     assign(:users, [
-      stub_model(User, id: 100, name: "hello"),
-      stub_model(User, id: 101, name: "goodbye")
+      stub_model(User, id: 100, name: "hello", daily_rating: "123"),
+      stub_model(User, id: 101, name: "goodbye", daily_rating: "345")
     ])
     view.stub!(:current_user).and_return(stub_model(User, id: 50))
     stub_template "_ranking_links.html.erb" => "<div>Ranking list</div>"
+    assign(:send, "daily_rating")
   end
 
   it "renders the ranking list" do
@@ -22,9 +23,11 @@ describe "users/index" do
     render
     within("#user_100") do
       rendered.should have_content("hello")
+      rendered.should have_content("123")
     end
     within("#user_101") do
       rendered.should have_content("goodbye")
+      rendered.should have_content("345")
     end
   end
 
