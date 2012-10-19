527ffc064636218c55a603741b93ccbd68e66d32
Grant Speelman <grant.speelman@unboxedconsulting.com>
2012-10-19 17:31:31 +0200

    new words added

--- config/initializers/load_dictionary.rb
+++ config/initializers/load_dictionary.rb
@@ -1,11 +1,7 @@
-File.readlines('db/words_with_no_clues.txt').each do |words|
-  words.split(/\s/).each do |word|
-    Dictionary.add(word)
-  end
-end
-# words with clues
-File.readlines('db/words_with_clues.csv').each do |line|
+# words
+File.readlines('db/words.csv').each do |line|
   word,clue = line.split(",",2)
-  Dictionary.set_clue(word,clue)
+  clue.squish!
+  Dictionary.set_clue(word,clue) unless clue.blank?
   Dictionary.add(word)
 end
