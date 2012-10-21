b766c613ac7745e5d5725a1b4d29d7ba05701660
Grant Speelman <grant.speelman@unboxedconsulting.com>
2012-10-21 12:16:17 +0200

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121018104819) do
+ActiveRecord::Schema.define(:version => 20121021084640) do
 
   create_table "games", :force => true do |t|
     t.string   "word"
@@ -86,6 +86,7 @@ ActiveRecord::Schema.define(:version => 20121018104819) do
     t.integer  "clue_points",       :default => 2, :null => false
     t.integer  "prize_points",      :default => 0, :null => false
     t.string   "login"
+    t.integer  "lock_version",      :default => 0, :null => false
   end
 
   add_index "users", ["created_at"], :name => "index_users_on_created_at"
