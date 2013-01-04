b9b830d90994deab1d5faa1a26eb2bad8802bc3c
Grant Speelman <grant.speelman@unboxedconsulting.com>
2013-01-04 17:12:38 +0200

    set airbrake api key

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,5 +1,5 @@
 Airbrake.configure do |config|
-  config.api_key = ENV['AIRBRAKE_API_KEY'] || '153f0678e4adcb5855b919b0118278e7'
+  config.api_key = '153f0678e4adcb5855b919b0118278e7'
   config.environment_name = ENV['AIRBRAKE_ENV_NAME'] if ENV['AIRBRAKE_ENV_NAME'].present?
   config.rescue_rake_exceptions = true
 end if ENV['AIRBRAKE_API_KEY'].present?
