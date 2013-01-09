4adf64e3a344563b461779738183f6c6b62e48e1
Grant Speelman <grant.speelman@unboxedconsulting.com>
2013-01-09 13:06:10 +0200

    fix up words spec

--- spec/requests/words_spec.rb
+++ spec/requests/words_spec.rb
@@ -6,12 +6,14 @@ describe 'words' do
   before :each do
     @current_user = create(:user, uid: 'm2604101', provider: 'mxit')
     add_headers('X_MXIT_USERID_R' => 'm2604101')
-    @body = [{'text' => "today is tomorrow"}].to_json
+    stub_shinka_request # stub shinka request
+    stub_google_tracking # stub google tracking
   end
 
   it "must allow to look up definition of the word" do
+    body = [{'text' => "today is tomorrow"}].to_json
     stub_request(:get, "http://api.wordnik.com/v4/word.json/today/definitions").
-      to_return(:status => 200, :body => @body, :headers => {})
+      to_return(:status => 200, :body => body, :headers => {})
     create(:won_game, word: "today", user: @current_user)
     visit '/'
     click_link 'show'
