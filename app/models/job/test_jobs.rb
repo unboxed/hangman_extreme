a8c3ecede7106de004f3972b8a53341d8e57ec71
Grant Speelman <grant.speelman@unboxedconsulting.com>
2013-03-06 17:13:20 +0200

    update job schedules

---
+++ app/models/job/test_jobs.rb
@@ -0,0 +1,19 @@
+module Job
+
+  class TestJobs
+
+    def run
+      # perform work here
+      APP_SETTINGS[:test_jobs_counter] ||= 0
+      APP_SETTINGS[:test_jobs_counter] += 1
+    end
+
+    def on_error(exception)
+      # Optionally implement this method to interrogate any exceptions
+      # raised inside the job's run method.
+      Airbrake.notify_or_ignore(exception,:cgi_data => ENV)
+    end
+
+  end
+
+end
