dd4d280060cc1bb4053603ebdcaa4588b92a3820
Grant Speelman <grant.speelman@unboxedconsulting.com>
2013-03-13 14:08:48 +0200

    cache define word with the layout

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,8 +1,9 @@
 class WordsController < ApplicationController
   before_filter :login_required
-  caches_action :define,  layout: false, expires_in: 7.days
+  caches_action :define, expires_in: 7.days
 
   def define
     @definition = Dictionary.define(params[:word])
   end
+
 end
