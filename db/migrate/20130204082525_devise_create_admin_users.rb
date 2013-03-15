40f374579f46dba2d6af4416b69fb861d4676fb5
Grant Speelman <grant.speelman@unboxedconsulting.com>
2013-03-15 22:52:11 +0200

    fix migration by removing for admin creation of active admin

--- db/migrate/20130204082525_devise_create_admin_users.rb
+++ db/migrate/20130204082525_devise_create_admin_users.rb
@@ -2,9 +2,9 @@ class DeviseCreateAdminUsers < ActiveRecord::Migration
   def migrate(direction)
     super
     # Create a default user
-    AdminUser.create!(:email => 'grant.speelman@ubxd.com',
-                      :password => 'password',
-                      :password_confirmation => 'password') if direction == :up
+    #AdminUser.create!(:email => 'grant.speelman@ubxd.com',
+    #                  :password => 'password',
+    #                  :password_confirmation => 'password') if direction == :up
   end
 
   def change
