d893ba68aa7cd541ad7f0a60fd2d3ad991125bef
Grant Speelman <grant.speelman@unboxedconsulting.com>
2013-03-16 22:05:19 +0200

    add more shinka adverts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -38,6 +38,30 @@ module ApplicationHelper
     end
   end
 
+  def shinka_img_ad
+    result = ""
+    return result unless shinka_ads_enabled?
+    begin
+      Timeout::timeout(5) do
+        headers = {"User-Agent" => "Mozilla Compatible/5.0 #{env['HTTP_USER_AGENT']}",
+                   "X-FORWARDED-FOR" => request_ip_address}
+        ads = ActiveSupport::JSON.decode(open("http://ox-d.shinka.sh/ma/1.0/arj?auid=#{shinka_auid}&c.age=#{current_user_request_info.age}&c.gender=#{current_user_request_info.gender}",headers).read)
+        return "" if ads['ads']['count'].to_i == 0
+        ads['ads']["ad"].sample["html"].gsub("href=","onclick='window.open(this.href); return false;' href=").html_safe
+      end
+    rescue Timeout::Error => time_error
+      Rails.logger.error(time_error.message)
+      Settings.shinka_disabled_until = 1.minute.from_now # disable for 1 minute
+      return result
+    rescue Exception => e
+      ENV['AIRBRAKE_API_KEY'].present? ? notify_airbrake(e) : Rails.logger.error(e.message)
+      Settings.shinka_disabled_until = 10.minutes.from_now # disable for a hour
+      raise unless Rails.env.production?
+      return result
+    end
+  end
+
+
   def shinka_ads_enabled?
     shinka_auid.present? &&
     (Settings.shinka_disabled_until.blank? || Time.current > Settings.shinka_disabled_until)
