9bd2ab0f4032040b1438b38aeeca02b727540591
Grant Speelman <grant.speelman@unboxedconsulting.com>
2013-03-18 16:56:45 +0200

    refactor shinka code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,63 +1,51 @@
 module ApplicationHelper
 
   def shinka_ad
-    result = ""
-    return result unless shinka_ads_enabled?
-    begin
-      Timeout::timeout(5) do
-        headers = {"User-Agent" => "Mozilla Compatible/5.0 #{env['HTTP_USER_AGENT']}",
-                   "X-FORWARDED-FOR" => request_ip_address}
-        ads = ActiveSupport::JSON.decode(open("http://ox-d.shinka.sh/ma/1.0/arj?auid=#{shinka_auid}&c.age=#{current_user_request_info.age}&c.gender=#{current_user_request_info.gender}",headers).read)
-        return "" if ads['ads']['count'].to_i == 0
-        ad = ads['ads']["ad"].sample
-        result = ad["html"].gsub("href=","onclick='window.open(this.href); return false;' href=").html_safe
-        creative = ad["creative"].first
-        tracking = creative["tracking"]
-        alt = creative["alt"]
-        beacon = "<div style='position: absolute; left: 0px; top: 0px; visibility: hidden;'>"
-        beacon << "<img src='#{tracking['impression']}' alt=\"\" height=\"1\" width=\"1\"/>"
-        beacon << "</div>"
-        if alt.blank? || alt.include?("http")
-          link = creative["media"]
-          return result if link.blank? || !link.include?("href=")
-          link.gsub!("href=","onclick='window.open(this.href); return false;' href=")
-          "#{beacon}#{link}".html_safe
-        else
-          "#{beacon}<a href=#{tracking['click']} onclick='window.open(this.href); return false;' >#{alt}</a>".html_safe
-        end
-      end
-    rescue Timeout::Error => time_error
-      Rails.logger.error(time_error.message)
-      Settings.shinka_disabled_until = 1.minute.from_now # disable for 1 minute
-      return result
-    rescue Exception => e
-      ENV['AIRBRAKE_API_KEY'].present? ? notify_airbrake(e) : Rails.logger.error(e.message)
-      Settings.shinka_disabled_until = 10.minutes.from_now # disable for a hour
-      raise unless Rails.env.production?
-      return result
+    return "" unless shinka_ads_enabled?
+    ad = get_shinka_ad
+    return "" if ad.empty?
+    result = ad["html"].gsub("href=","onclick='window.open(this.href); return false;' href=").html_safe
+    creative = ad["creative"].first
+    tracking = creative["tracking"]
+    alt = creative["alt"]
+    beacon = "<div style='position: absolute; left: 0px; top: 0px; visibility: hidden;'>"
+    beacon << "<img src='#{tracking['impression']}' alt=\"\" height=\"1\" width=\"1\"/>"
+    beacon << "</div>"
+    if alt.blank? || alt.include?("http")
+      link = creative["media"]
+      return result if link.blank? || !link.include?("href=")
+      link.gsub!("href=","onclick='window.open(this.href); return false;' href=")
+      "#{beacon}#{link}".html_safe
+    else
+      "#{beacon}<a href=#{tracking['click']} onclick='window.open(this.href); return false;' >#{alt}</a>".html_safe
     end
   end
 
   def shinka_img_ad
-    result = ""
-    return result unless shinka_ads_enabled?
+    return "" unless shinka_ads_enabled?
+    ad = get_shinka_ad
+    return "" if ad.empty?
+    ad["html"].gsub("href=","onclick='window.open(this.href); return false;' href=").html_safe
+  end
+
+  def get_shinka_ad
     begin
       Timeout::timeout(5) do
         headers = {"User-Agent" => "Mozilla Compatible/5.0 #{env['HTTP_USER_AGENT']}",
                    "X-FORWARDED-FOR" => request_ip_address}
         ads = ActiveSupport::JSON.decode(open("http://ox-d.shinka.sh/ma/1.0/arj?auid=#{shinka_auid}&c.age=#{current_user_request_info.age}&c.gender=#{current_user_request_info.gender}",headers).read)
-        return "" if ads['ads']['count'].to_i == 0
-        ads['ads']["ad"].sample["html"].gsub("href=","onclick='window.open(this.href); return false;' href=").html_safe
+        return {} if ads['ads']['count'].to_i == 0
+        ads['ads']["ad"].sample
       end
     rescue Timeout::Error => time_error
       Rails.logger.error(time_error.message)
       Settings.shinka_disabled_until = 1.minute.from_now # disable for 1 minute
-      return result
+      return {}
     rescue Exception => e
       ENV['AIRBRAKE_API_KEY'].present? ? notify_airbrake(e) : Rails.logger.error(e.message)
       Settings.shinka_disabled_until = 10.minutes.from_now # disable for a hour
       raise unless Rails.env.production?
-      return result
+      return {}
     end
   end
 
