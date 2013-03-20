7dc91baae4dc4f17dc504ce653900051bb679c12
Grant Speelman <grant.speelman@unboxedconsulting.com>
2013-03-20 16:46:17 +0200

    remove jenkins scripts

--- script/jenkins_deploy_staging.sh
+++
@@ -1,7 +0,0 @@
-eval `ssh-agent -s`
-ssh-add
-rvm use 1.9.3@hangman_extreme
-git push staging master
-heroku run rake db:migrate --app tryhangmanleague
-heroku run rake airbrake:deploy TO=staging --app tryhangmanleague
-
