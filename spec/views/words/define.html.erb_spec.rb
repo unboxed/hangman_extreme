945921fc8450e715b068f33853489f241504cdf4
Grant Speelman <grant.speelman@unboxedconsulting.com>
2013-04-10 15:01:05 +0200

    remove home and play links on define page

--- spec/views/words/define.html.erb_spec.rb
+++ spec/views/words/define.html.erb_spec.rb
@@ -13,18 +13,8 @@ describe "words/define.html.erb" do
     rendered.should have_content(@definition)
   end
 
-  it "must have new game link on menu" do
-    view.should_receive(:menu_item).with(anything,new_game_path,id: 'new_game')
-    render
-  end
-
   it "should have a view rank link on menu" do
     view.should_receive(:menu_item).with(anything,user_path(50),id: 'view_rank')
-    render
-  end
-
-  it "should have a home page link" do
-    view.should_receive(:menu_item).with(anything,root_path,id: 'root_page')
     render
   end
 
