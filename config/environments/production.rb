5607f4c642954cf70a7f0a4fe289a8e5258de9a9
Grant Speelman <grant.speelman@unboxedconsulting.com>
2013-04-10 12:54:35 +0200

    fix secure assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ HangmanLeague::Application.configure do
   # config.cache_store = :torque_box_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  config.action_controller.asset_host = "http://hmxassets.speelman.co.za"
+  # config.action_controller.asset_host = "http://hmxassets.speelman.co.za"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( active_admin.css active_admin.js active_admin/print.css )
