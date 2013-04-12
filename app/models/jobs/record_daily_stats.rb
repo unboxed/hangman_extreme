b5f9aad487a182b6ae582e0c76a26639a0fe2a85
Grant Speelman <grant.speelman@unboxedconsulting.com>
2013-04-12 16:49:27 +0200

    fix stats

--- app/models/jobs/record_daily_stats.rb
+++ app/models/jobs/record_daily_stats.rb
@@ -6,7 +6,7 @@ class Jobs::RecordDailyStats < Jobs::Base
     queue.add :purchases_per_day => PurchaseTransaction.last_day.sum(:moola_amount)
     queue.add :redeem_winning_per_day => RedeemWinning.last_day.sum(:prize_amount)
     queue.add :active_mxit_per_day => User.active_mxit_today.count
-    queue.add :active_mxit_per_day => User.active_non_mxit.count
+    queue.add :active_non_mxit_per_day => User.active_non_mxit_today.count
     queue.submit
   end
 
