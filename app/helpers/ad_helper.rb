4e2a3d26a4c3d586bcfbed3ed30f49a28a6fdda5
Grant Speelman <grant.speelman@unboxedconsulting.com>
2013-05-16 10:25:58 +0200

    Increase shinka ad timeout

--- app/helpers/ad_helper.rb
+++ app/helpers/ad_helper.rb
@@ -49,7 +49,7 @@ module AdHelper
 
   def get_shinka_ad
     begin
-      Timeout::timeout(5) do
+      Timeout::timeout(10) do
         headers = {"User-Agent" => "Mozilla Compatible/5.0 #{env['HTTP_USER_AGENT']}",
                    "X-FORWARDED-FOR" => request_ip_address}
         ads = ActiveSupport::JSON.decode(open("http://ox-d.shinka.sh/ma/1.0/arj?auid=#{shinka_auid}&c.age=#{current_user_request_info.age}&c.gender=#{current_user_request_info.gender}",headers).read)
@@ -58,7 +58,7 @@ module AdHelper
       end
     rescue Timeout::Error => time_error
       Rails.logger.error(time_error.message)
-      Settings.shinka_disabled_until = 1.minute.from_now # disable for 1 minute
+      Settings.shinka_disabled_until = 20.seconds.from_now # disable for 20 seconds
       return {}
     rescue Exception => e
       ENV['AIRBRAKE_API_KEY'].present? ? notify_airbrake(e) : Rails.logger.error(e.message)
