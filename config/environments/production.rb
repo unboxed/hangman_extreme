6518f024bf3f119950229d777b9cb42e6e2a241b
Grant Speelman <grant.speelman@unboxedconsulting.com>
2013-05-29 15:36:24 +0200

    fixed fallback to assets pipeline if a precompiled asset is missed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,7 @@ HangmanLeague::Application.configure do
   config.assets.compress = true
 
   # fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = trye
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
