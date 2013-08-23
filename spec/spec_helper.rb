9228531540fcf01f8d977d1f4484506e81e1838c
Grant Speelman <grant.speelman@ubxd.com>
2013-08-23 08:33:08 +0200

    fix up  show and hide the hangman user spec and don't use puma in testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -86,11 +86,11 @@ Spork.each_run do
     c.ignore_localhost = true
   end
 
-  Capybara.server do |app, port|
-    Puma::Server.new(app).tap do |s|
-      s.add_tcp_listener '127.0.0.1', port
-    end.run.join
-  end
+  #Capybara.server do |app, port|
+  #  Puma::Server.new(app).tap do |s|
+  #    s.add_tcp_listener '127.0.0.1', port
+  #  end.run.join
+  #end
 
   RSpec.configure do |config|
     config.include FactoryGirl::Syntax::Methods
