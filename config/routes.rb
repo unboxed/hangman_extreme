8034ca97b39dc95e44ff17fe62f7be1b39700221
Grant Speelman <grant.speelman@ubxd.com>
2013-10-30 08:22:55 +0200

    update feeback routing to /user/feedback

--- config/routes.rb
+++ config/routes.rb
@@ -27,12 +27,13 @@ HangmanLeague::Application.routes.draw do
       get 'show_hangman', action: 'show_hangman', as: 'show_hangman'
     end
   end
-  resources :feedback, :except => [:show, :edit, :update, :destroy]
+  resources :feedback, path: "user/feedback", :except => [:show, :edit, :update, :destroy]
   resources :winners, :except => [:edit, :update, :create, :new, :destroy]
   resources :redeem_winnings, :except => [:edit, :update, :destroy]
 
   get '/define/:word', to: 'words#define', as: 'define_word'
   get '/auth/:provider/callback', to: 'sessions#create'
+  post '/auth/:provider/callback', to: 'sessions#create'
   get '/auth/:provider/failure', to: 'sessions#failure'
   get '/auth/failure', to: 'sessions#failure'
   get '/authorize', to: 'users#mxit_authorise', as: 'mxit_authorise'
--- spec/routing/feedback_routing_spec.rb
+++ spec/routing/feedback_routing_spec.rb
@@ -4,15 +4,15 @@ describe FeedbackController do
   describe "routing" do
 
     it "routes to #index" do
-      get("/feedback").should route_to("feedback#index")
+      get("/user/feedback").should route_to("feedback#index")
     end
 
     it "routes to #new" do
-      get("/feedback/new").should route_to("feedback#new")
+      get("/user/feedback/new").should route_to("feedback#new")
     end
 
     it "routes to #create" do
-      post("/feedback").should route_to("feedback#create")
+      post("/user/feedback").should route_to("feedback#create")
     end
 
   end
