49945e60d93d7d515fed3fc4b29b8e71c87d2b87
Grant Speelman <grant.speelman@ubxd.com>
2013-12-10 11:01:43 +0200

    cancel winnings for users not registerd on mxit money

--- app/workers/issue_mxit_money_to_users.rb
+++ app/workers/issue_mxit_money_to_users.rb
@@ -6,16 +6,20 @@ class IssueMxitMoneyToUsers < IssueWinningToUser
     # do something
     if connection.balance >= redeem_winning.prize_amount
       user_mxit_info = connection.user_info(:id => redeem_winning.user_uid)
-      result = connection.issue_money(:phone_number => user_mxit_info[:msisdn],
-                                      :merchant_reference => "RW#{redeem_winning.id}Y#{Time.current.yday}H#{Time.current.hour}",
-                                      :amount_in_cents => redeem_winning.prize_amount)
-      if result && result[:m2_reference]
-        redeem_winning.paid!
-        redeem_winning.update_column(:mxit_money_reference,result[:m2_reference])
+      if user_mxit_info[:is_registered]
+        result = connection.issue_money(:phone_number => user_mxit_info[:msisdn],
+                                        :merchant_reference => "RW#{redeem_winning.id}Y#{Time.current.yday}H#{Time.current.hour}",
+                                        :amount_in_cents => redeem_winning.prize_amount)
+        if result && result[:m2_reference]
+          redeem_winning.paid!
+          redeem_winning.update_column(:mxit_money_reference,result[:m2_reference])
+        else
+          exception_msg = "Invalid Result from Mxit Api: #{result.inspect}"
+          exception_msg = "#{result[:error_type]}: #{result[:message]}" if result && result[:error_type]
+          raise(exception_msg)
+        end
       else
-        exception_msg = "Invalid Result from Mxit Api: #{result.inspect}"
-        exception_msg = "#{result[:error_type]}: #{result[:message]}" if result && result[:error_type]
-        raise(exception_msg)
+        redeem_winning.cancel!
       end
     else
       raise("No enough mxit money: balance:#{connection.balance} >= prize_amount:#{redeem_winning.prize_amount}")
