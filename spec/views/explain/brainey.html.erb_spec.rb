ad4610049992698e4a191ff8834d36f4a4244d96
Concetta Isolano <concetta.isolano@gmail.com>
2013-12-12 16:45:56 +0200

    Add views for Brainey Badge

--- spec/views/explain/brainey.html.erb_spec.rb
+++ spec/views/explain/brainey.html.erb_spec.rb
@@ -3,7 +3,9 @@ require 'spec_helper'
 describe "explain/brainey.html.erb" do 
 
 	before(:each) do
+		@user = create(:user)
 		view.stub!(:menu_item)
+		view.stub!(:current_user).and_return(@user)
 	end
 
 	it "must have a link to badges" do
@@ -14,6 +16,17 @@ describe "explain/brainey.html.erb" do
 	it "must describe how to obtain badge brainey" do
 		render
 		rendered.should have_content('row')
+	end 
+
+	it "must show badge if 10 games were won and no clues used" do 
+		create(:badge, user: @user, name: 'Brainey')
+		render
+		rendered.should have_content('Achieved this Badge')
+	end
+
+	it "must not show badge if <10 games were won in a row" do 
+		render
+		rendered.should have_content('Still to Achieve')
 	end 
 
 end 
