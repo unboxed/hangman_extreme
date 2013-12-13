c4b8aba61a8174dc3f83cffedae6f53cfd6d54c5
Grant Speelman <grant.speelman@ubxd.com>
2013-12-13 11:28:57 +0200

    no production enviroment on heroku anymore

--- script/deploy_production.sh
+++
@@ -1,6 +0,0 @@
-git remote add production git@heroku.com:mxithangmanleague.git
-heroku maintenance:on --app mxithangmanleague && \
-git push production master && \
-heroku run rake db:migrate --app mxithangmanleague && \
-heroku run rake airbrake:deploy TO=production --app mxithangmanleague && \
-heroku maintenance:off --app mxithangmanleague
