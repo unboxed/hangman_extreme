38be68c39028c0a9bfeeb17d0e8dce1347f643cd
Concetta Isolano <concetta.isolano@gmail.com>
2013-12-18 12:59:25 +0200

    Change Deploy staging for heroku

--- script/deploy_staging.sh
+++ script/deploy_staging.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 git remote add staging git@heroku.com:tryhangmanleague.git
 heroku maintenance:on --app tryhangmanleague && \
-<<<<<<< HEAD
 git push staging badges:master --force --verbose && \
-=======
-git push staging badges:master --verbose && \
->>>>>>> a3e4f3ca317d2275f67fa4042e56235b0227551f
 heroku run rake db:migrate --app tryhangmanleague && \
 heroku run rake airbrake:deploy TO=staging --app tryhangmanleague && \
 heroku maintenance:off --app tryhangmanleague
