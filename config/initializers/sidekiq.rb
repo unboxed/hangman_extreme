31219f83d83f194e1f7840745914b2698763794c
Grant Speelman <grant.speelman@ubxd.com>
2014-01-20 13:58:21 +0200

    can set sidekiq size

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,9 @@
 require 'sidekiq'
+redis_options = { :url => ENV['SIDEKIQ_REDIS_URL'], :namespace => 'sidekiq' }
+redis_options[:size] = ENV['SIDEKIQ_SIZE'].to_i if ENV['SIDEKIQ_SIZE'].present?
 Sidekiq.configure_server do |config|
-  config.redis = { :url => ENV['SIDEKIQ_REDIS_URL'], :namespace => 'sidekiq' }
+  config.redis = redis_options
 end
 Sidekiq.configure_client do |config|
-  config.redis = { :url => ENV['SIDEKIQ_REDIS_URL'], :namespace => 'sidekiq' }
+  config.redis = redis_options
 end
