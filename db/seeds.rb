9a41087a115703e3c142170d3df0a541c0c1d6df
Grant Speelman <grant.speelman@ubxd.com>
2014-01-20 12:12:05 +0200

    update seed code

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,29 @@
 if PurchaseTransaction.where('user_account_id IS NULL').any?
-  puts "Migrating purchase transactions to UserAccount"
+  puts "Migrating #{PurchaseTransaction.where('user_account_id IS NULL').count} purchase transactions to UserAccount"
   while PurchaseTransaction.where('user_account_id IS NULL').any?
-    PurchaseTransaction.where('user_account_id IS NULL').limit(100).each do |purchase_transaction|
+    PurchaseTransaction.where('user_account_id IS NULL').limit(1000).each do |purchase_transaction|
       purchase_transaction.update_attributes( :user_account => User.find(purchase_transaction._deprecated_user_id).account )
+      print "."
     end
   end
 end
 
 if RedeemWinning.where('user_account_id IS NULL').any?
-  puts "Migrating redeem winnings to UserAccount"
+  puts "Migrating #{RedeemWinning.where('user_account_id IS NULL').count} redeem winnings to UserAccount"
   while RedeemWinning.where('user_account_id IS NULL').any?
-    RedeemWinning.where('user_account_id IS NULL').limit(100).each do |rw|
+    RedeemWinning.where('user_account_id IS NULL').limit(1000).each do |rw|
       rw.update_attributes( :user_account => User.find(rw._deprecated_user_id).account )
+      print "."
     end
   end
 end
 
 if AirtimeVoucher.where('user_account_id IS NULL').any?
-  puts "Migrating airtime_voucher to UserAccount"
+  puts "Migrating #{AirtimeVoucher.where('user_account_id IS NULL').count} airtime vouchers to UserAccount"
   while AirtimeVoucher.where('user_account_id IS NULL').any?
-    AirtimeVoucher.where('user_account_id IS NULL').limit(100).each do |av|
+    AirtimeVoucher.where('user_account_id IS NULL').limit(1000).each do |av|
       av.update_attributes( :user_account => User.find(av._deprecated_user_id).account )
+      print "."
     end
   end
 end
